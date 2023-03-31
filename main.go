2d19528b019debf8969f467aaa5b4224b89022a6
Mo <mohammadreza@arize.io>
2023-03-31 22:57:45 +0400

    add echo package

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"fmt"
-)
-func main() {
+	"net/http"
 
-	fmt.Println("this is a test")
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", hello)
+
+	e.Logger.Fatal(e.Start(":1323"))
+
+	// fmt.Println("this is a test")
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello From Echo library!")
 }
