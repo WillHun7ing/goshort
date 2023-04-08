be5a96687c57a554245868a511615e4e17c48166
Mo <mohammadreza@arize.io>
2023-04-09 02:18:09 +0400

    check if link already exist

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/teris-io/shortid"
 
@@ -14,16 +13,84 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-
-	"goshort/database"
 )
 
 type Link struct {
-	Long  string `json:"long" xml:"long"`
-	Short string `json:"short" xml:"short"`
-	Visit uint32 `json:"visit" xml:"visit"`
+	Long  string `json:"long" bson:"long" xml:"long"`
+	Short string `json:"short" bson:"short" xml:"short"`
+	Visit uint32 `json:"visit" bson:"visit" xml:"visit"`
+}
+
+var collection *mongo.Collection
+
+// var ctx = context.TODO()
+
+// func init() {
+// 	mongoUri := fmt.Sprintf("mongodb://%s:%s@%s:%s/", os.Getenv("MONGO_ROOT_USERNAME"), os.Getenv("MONGO_ROOT_PASSWORD"), os.Getenv("MONGO_HOST"), os.Getenv("MONGO_PORT"))
+// 	clientOptions := options.Client().ApplyURI(mongoUri)
+// 	client, err := mongo.Connect(ctx, clientOptions)
+
+// 	if err != nil {
+// 		log.Fatal("Mongo init, ", err)
+// 	}
+// 	err = client.Ping(ctx, nil)
+// 	if err != nil {
+// 		log.Fatal("Mongo client pign, ", err)
+// 	}
+// 	collection = client.Database("goshort").Collection("links")
+// }
+
+var ctx context.Context
+var cancel context.CancelFunc
+
+func createLink(link *Link) error {
+	_, err := collection.InsertOne(ctx, link)
+	if err != nil {
+		log.Fatalln("Insert error: ", err)
+	}
+	return err
+}
+
+func getAll() ([]*Link, error) {
+	// passing bson.D{{}} matches all documents in the collection
+	filter := bson.D{{}}
+	return filterTasks(filter)
+}
+
+func filterTasks(filter interface{}) ([]*Link, error) {
+	// A slice of tasks for storing the decoded documents
+	var tasks []*Link
+
+	cur, err := collection.Find(ctx, filter)
+	if err != nil {
+		return tasks, err
+	}
+
+	for cur.Next(ctx) {
+		var t Link
+		err := cur.Decode(&t)
+		if err != nil {
+			return tasks, err
+		}
+
+		tasks = append(tasks, &t)
+	}
+
+	if err := cur.Err(); err != nil {
+		return tasks, err
+	}
+
+	// once exhausted, close the cursor
+	cur.Close(ctx)
+
+	if len(tasks) == 0 {
+		return tasks, mongo.ErrNoDocuments
+	}
+
+	return tasks, nil
 }
 
 func main() {
@@ -36,10 +103,17 @@ func main() {
 	sid, err := shortid.New(1, shortid.DefaultABC, 2342)
 	shortid.SetDefault(sid)
 
-	ctx, collection, err := mongoConnect()
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	ctx := context.Background()
+	// defer cancel()
+	// mongoUri := fmt.Sprintf("mongodb://%s:%s@%s:%s/", os.Getenv("MONGO_ROOT_USERNAME"), os.Getenv("MONGO_ROOT_PASSWORD"), os.Getenv("MONGO_HOST"), os.Getenv("MONGO_PORT"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://127.0.0.1:27017/"))
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	collection = client.Database("goshort").Collection("links")
 
 	e := echo.New()
 
@@ -48,27 +122,44 @@ func main() {
 
 	e.POST("/short", func(c echo.Context) error {
 		url := c.FormValue("url")
-		short, err := shortid.Generate()
+		var result Link
+		err := collection.FindOne(ctx, bson.D{{"long", url}}).Decode(&result)
 		if err != nil {
-			log.Fatalln(err)
+			short, err := shortid.Generate()
+			if err != nil {
+				log.Fatalln(err)
+			}
+			l := &Link{
+				Long:  url,
+				Short: short,
+				Visit: 0,
+			}
+			createLink(l)
+			return c.JSON(http.StatusOK, l)
 		}
-		l := &Link{
-			Long:  url,
-			Short: short,
-			Visit: 0,
-		}
-		l.insertOnDb(ctx, collection)
-		return c.JSON(http.StatusOK, l)
-	})
-	// e.POST("/:shortUrl", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, result)
 
+	})
+
+	// e.POST("/:shortUrl", func(c echo.Context) error {
+	// 	shortUrl := c.Param("shortUrl")
+	// 	return c.JSON(http.StatusOK, shortUrl)
 	// })
+
+	e.GET("/", func(c echo.Context) error {
+		// links, err := getAll()
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		return c.JSON(http.StatusOK, &Link{})
+	})
 
 	port := fmt.Sprintf(":%s", os.Getenv("APP_PORT"))
 	e.Logger.Fatal(e.Start(port))
 }
 
-func mongoConnect() (context.Context, *mongo.Collection, error) {
+/*
+func mongoConnect() (*context.Context, *mongo.Collection, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	mongoUri := fmt.Sprintf("mongodb://%s:%s@%s:%s/", os.Getenv("MONGO_ROOT_USERNAME"), os.Getenv("MONGO_ROOT_PASSWORD"), os.Getenv("MONGO_HOST"), os.Getenv("MONGO_PORT"))
@@ -78,18 +169,23 @@ func mongoConnect() (context.Context, *mongo.Collection, error) {
 	}
 	defer client.Disconnect(ctx)
 
-	db := database.CreateDatabase(client, "goshort")
-	c := db.CreateCollection("links")
+	// db := database.CreateDatabase(client, "goshort")
+	// c := db.CreateCollection("links")
 
-	return ctx, c, nil
+	connection := client.Database("goshort")
+	c := connection.Collection("links")
+
+	return &ctx, c, nil
 }
 
-func (data *Link) insertOnDb(ctx context.Context, c *mongo.Collection) error {
-	res, err := c.InsertOne(ctx, data)
+func (data *Link) insertOnDb(ctx *context.Context, c *mongo.Collection) error {
+	res, err := c.InsertOne(*ctx, data)
 	if err != nil {
 		return err
 	}
 	id := res.InsertedID
 	log.Printf("Insert ID, %s", id)
+	fmt.Println("😈😈😈😈 Insert Id", id)
 	return nil
 }
+*/
